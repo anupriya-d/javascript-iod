198aa2e715407c195bbe19f3be991bc604fbed1d
Anupriya Dayaratne <anupriya.dayaratne@gmail.com>
2025-10-18 15:48:36 +1300

    examples added

---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+console.log('------------------Data Types - Numbers -------------------- ');
+
+let four = 4 ; 
+
+four = 'four';
+
+// Infinity = 1/0
+// -Infinity = -1/0
+
+
+let Four = 44 ;
+
+console.log(four);
+console.log(Four);
+
+let infinity = 1/0;
+
+let negInfinity = -1/0;
+
+
+
+console.log(infinity == Infinity); // must be true 
+
+console.log(negInfinity == -Infinity) //must be true
+
+let nan = 'Text'/10;
+
+console.log(nan); // NaN -> Not a number
+
+console.log (nan == NaN); // NaN != NaN ,, so it is false 
+
+console.log(isNaN(nan)); // you can check using function isNaN
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,51 @@
+console.log('------Javascprit-----------------');
+
+//one line comment 
+
+
+/* Multi
+
+Line 
+
+Comment */
+
+
+if(true) {
+    var oldVariable = "Don't use var" ;
+}
+
+console.log(oldVariable);
+
+
+/* variable naming
+
+can only contain letters,digits or symbol $ and _
+the first charactor cannot be a digit 
+reserved names cannot be used ... ex - let,const,class
+name is case sensitive
+Non-Latin letters allow but not recommended 
+
+*/
+
+let firstName = 'Anupriya';   
+
+const lastName = 'Dayaratne';
+
+console.log(firstName);
+console.log(lastName);
+
+
+
+
+ let city = 'Dunedin'
+
+city = 'Auckland';
+
+//in let can re-assign but not redeclare the variable name
+
+console.log(city);
+
+
+
+
+
